99ed9ba0bcc3e5d369707859a286130beec86e89
William Ferreira de Paula <739447+dinowilliam@users.noreply.github.com>
2022-06-30 23:32:13 -0300

    Add Result Tests

---
+++ FineCodeCoverageSpikeTest/ResultTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+using Moq;
+using FineCodeCoverageSpikeLibrary.Contracts;
+using FineCodeCoverageSpikeLibrary;
+
+namespace FineCodeCoverageSpikeTest {
+
+    [TestFixture]
+    public class ResultTests {
+        
+        IResult result;
+
+        [SetUp]
+        public void SetUp() {
+            
+        }
+
+        [Test]
+        public void Result_WhenResultReceiveAValue_IsSucceed() {
+
+            //Arange
+            result = new Result();
+
+            //Act
+            result.Result = 1;
+
+            //Assert
+            Assert.AreEqual(result.Result, 1);
+        }
+
+        [Test]
+        public void Result_WhenResultReceiveAValue_IsFailed() {
+
+            //Arange
+            var mockResult = new Mock<IResult>();
+            mockResult.SetupSet(x => x.Result = 1).Throws(new NullReferenceException());
+            result = mockResult.Object;
+
+            //Act
+            //Assert
+            Assert.Throws<NullReferenceException>(() => result.Result = 1);
+
+        }
+        
+    }
+}
